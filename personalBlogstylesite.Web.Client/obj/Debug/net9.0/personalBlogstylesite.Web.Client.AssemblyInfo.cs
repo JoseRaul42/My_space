cf160b2048aa36bc7f4542199963d5cdce72ee5b
JoseRaul42 <89216049+JoseRaul42@users.noreply.github.com>
2025-09-21 11:57:46 -0500

    newest changes

--- personalBlogstylesite.Web.Client/obj/Debug/net9.0/personalBlogstylesite.Web.Client.AssemblyInfo.cs
+++ personalBlogstylesite.Web.Client/obj/Debug/net9.0/personalBlogstylesite.Web.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personalBlogstylesite.Web.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e66b031d869a9ba212525ffc0a0ee8b7a64fc77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490710d3ddfbd9e506cf2481bea6b894d0630202")]
 [assembly: System.Reflection.AssemblyProductAttribute("personalBlogstylesite.Web.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personalBlogstylesite.Web.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
